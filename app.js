0039c229037e0f3d801618f0bdca41f37661732e
AnthonyTB <Anthonylol@protonmail.com>
2019-10-25 12:51:56 -0700

    added some html and css

---
+++ app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const questionStorage = [
+  {
+    question: 'Who is the Colts offical owner?',
+    answers: [ 'Mark Cuban', 'Jeff Bezos', 'Tim Cook' ],
+    correctAnswer: 'Jim Irsay'
+  },
+  {
+    question: 'What are the Colts offical team colors?',
+    answers: [ 'Light blue & Yellow', 'Red & Silver', 'Blue & Silver' ],
+    correctAnswer: 'Blue & White'
+  },
+  {
+    question: 'What is the Colts mascot name?',
+    answers: [ 'Horsey', 'Skippy', 'Rider' ],
+    correctAnswer: 'Blue'
+  }
+];
