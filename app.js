28ec8d3c8fd4b97e83109917f4a7b7d3e5b2df0d
therealmbittarelli <maggiekmcclellan@gmail.com>
2019-10-25 16:54:24 -0400

    added comments and function stubs

--- app.js
+++ app.js
@@ -1,19 +1,75 @@
 'use strict';
 
-const questionStorage = [
-  {
-    question: 'Who is the Colts offical owner?',
-    answers: [ 'Mark Cuban', 'Jeff Bezos', 'Tim Cook' ],
-    correctAnswer: 'Jim Irsay'
-  },
-  {
-    question: 'What are the Colts offical team colors?',
-    answers: [ 'Light blue & Yellow', 'Red & Silver', 'Blue & Silver' ],
-    correctAnswer: 'Blue & White'
-  },
-  {
-    question: 'What is the Colts mascot name?',
-    answers: [ 'Horsey', 'Skippy', 'Rider' ],
-    correctAnswer: 'Blue'
-  }
-];
+// const questionStorage = [
+//   {
+//     question: 'Who is the Colts offical owner?',
+//     answers: [ 'Mark Cuban', 'Jeff Bezos', 'Tim Cook' ],
+//     correctAnswer: 'Jim Irsay'
+//   },
+//   {
+//     question: 'What are the Colts offical team colors?',
+//     answers: [ 'Light blue & Yellow', 'Red & Silver', 'Blue & Silver' ],
+//     correctAnswer: 'Blue & White'
+//   },
+//   {
+//     question: 'What is the Colts mascot name?',
+//     answers: [ 'Horsey', 'Skippy', 'Rider' ],
+//     correctAnswer: 'Blue'
+//   }
+// ];
+
+// resets score and question # counters
+function resetStats() {
+
+}
+
+// updates the score counter each time a question is answered correctly
+function updateScore() {
+
+}
+
+// updates the counter displaying how many questions the user has answered out of the total # of questions
+function updateQuestionNum() {
+
+}
+
+// allows user to start the quiz
+function beginQuiz() {
+
+}
+
+// user submits an answer
+function submitAnswer() {
+
+}
+
+// generates HTML for the quiz form
+function generateForm() {
+
+}
+
+// the display shown if user answers correctly; flash lighter blue
+function correct() {
+
+}
+
+// the display shown if user answers incorrectly; flash silver
+function wrong() {
+
+}
+
+// generates next question
+function nextQuestion() {
+
+}
+
+// generates final screen, including score, and congratulations or defeat message
+function results() {
+
+}
+
+
+// gives the user the chance to take the quiz again from the beginning, once they have completed the quiz
+function restartQuiz() {
+
+}
