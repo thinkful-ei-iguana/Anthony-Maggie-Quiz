548de004ee235920edea28276c074332d60e7fa7
AnthonyTB <Anthonylol@protonmail.com>
2019-10-25 14:37:26 -0700

    update

--- app.js
+++ app.js
@@ -1,19 +1,95 @@
 'use strict';
 
-const questionStorage = [
-  {
-    question: 'Who is the Colts offical owner?',
-    answers: [ 'Mark Cuban', 'Jeff Bezos', 'Tim Cook' ],
-    correctAnswer: 'Jim Irsay'
-  },
-  {
-    question: 'What are the Colts offical team colors?',
-    answers: [ 'Light blue & Yellow', 'Red & Silver', 'Blue & Silver' ],
-    correctAnswer: 'Blue & White'
-  },
-  {
-    question: 'What is the Colts mascot name?',
-    answers: [ 'Horsey', 'Skippy', 'Rider' ],
-    correctAnswer: 'Blue'
-  }
-];
+// const questionStorage = [
+//   {
+//     question: 'Who is the Colts offical owner?',
+//     answers: [ 'Mark Cuban', 'Jeff Bezos', 'Tim Cook' ],
+//     correctAnswer: 'Jim Irsay'
+//   },
+//   {
+//     question: 'What are the Colts offical team colors?',
+//     answers: [ 'Light blue & Yellow', 'Red & Silver', 'Blue & Silver' ],
+//     correctAnswer: 'Blue & White'
+//   },
+//   {
+//     question: 'What is the Colts mascot name?',
+//     answers: [ 'Horsey', 'Skippy', 'Rider' ],
+//     correctAnswer: 'Blue'
+//   }
+// ];
+
+
+// Render!
+function renderQuiz() {
+    // html elements go here
+}
+
+
+// resets score and question # counters
+function resetStats() {
+    // will make sure that score and question # counters are empty at the start of the quiz
+    $('#start').on('submit', (e) =>
+        STORE.score = 0;
+    STORE.currentQuestion = 0;
+    renderQuiz();
+    console.log("render stats is working")
+  )
+}
+
+// updates the score counter each time a question is answered correctly
+function updateScore() {
+    // 1++ to STORE.score each time user correctly answers a questions (function correct() called)
+}
+
+// updates the counter displaying how many questions the user has answered out of the total # of questions
+function updateQuestionNum() {
+    // 1++ to STORE.currentQuestion each time user reaches a new question (function nextQuestion())
+}
+
+// allows user to start the quiz
+function beginQuiz() {
+    // when user hits start button, resetStats is called and quiz is rendered
+    $('#start').on('submit', (e) => {
+        generateQuestion();
+    })
+}
+
+function renderQuestion() {
+
+}
+
+// user submits an answer (not -- on.('click') --)
+function submitAnswer() {
+    // when user submits an answer, grab input
+
+    // the display shown if user answers correctly
+    function correct() {
+        // after submitAnswer, if correct/check against properties in the STORE/, display message stating so, and flash the box? window? a lighter blue
+        // call update score
+
+    }
+
+    // the display shown if user answers incorrectly; flash silver
+    function wrong() {
+        // after submitAnswer, if incorrect/check against properties in the STORE/, display message stating so, and flash the box? window? a lighter blue
+    }
+
+    // generates next question
+    function nextQuestion() {
+        // when user clicks the Next button, move to the next object in the store
+        // add if else statement: if there are no more questions left, call function results()
+    }
+
+    // generates final screen, including score, and congratulations or defeat message
+    function results() {
+        // display score and currentQuestion counters
+        // display final message based on score?
+        // display restart button
+    }
+
+
+    // gives the user the chance to take the quiz again from the beginning, once they have completed the quiz
+    function restartQuiz() {
+        // clicking restart button resets score and currentQuestion counters
+        // re-renders quiz from the beginning
+    } 
